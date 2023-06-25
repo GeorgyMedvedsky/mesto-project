37e3c2b2efd76dcdf109cf9651e2694b91bd44fa
Georgy Medvedsky <topchugora@gmail.com>
2023-06-25 21:54:18 +0300

    feat: добавить валидацию форм

--- scripts/index.js
+++ scripts/index.js
@@ -116,3 +116,49 @@ addButton.addEventListener('click', () => {
 });
 profileForm.addEventListener('submit', setProfileFormSumbit);
 newPlaceForm.addEventListener('submit', setNewPlaceFormSubmit);
+
+//Validation
+
+function showInputError(formElement, inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('popup__input_type_error');
+    errorElement.textContent = errorMessage;
+}
+function hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('popup__input_type_error');
+    errorElement.textContent = '';
+}
+function isValid(formElement, inputElement) {
+    if (inputElement.validity.patternMismatch) inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+    else inputElement.setCustomValidity("");
+
+    if(!inputElement.validity.valid) showInputError(formElement, inputElement, inputElement.validationMessage)
+    else hideInputError(formElement, inputElement);
+}
+function setEventListeners(formElement) {
+    const inputList = Array.from(formElement.elements);
+    const buttonElement = formElement.querySelector('.popup__submit');
+    toggleButtonState(inputList, buttonElement);
+    inputList.forEach(inputElement => inputElement.addEventListener('input', () => {
+        isValid(formElement, inputElement);
+        toggleButtonState(inputList, buttonElement);
+    }));
+}
+function enableValidation() {
+    const formList = Array.from(document.forms);
+    formList.forEach(formElement => setEventListeners(formElement));
+}
+function hasInvalidInput(inputList) {
+    return inputList.some(inputElement => !inputElement.validity.valid);
+}
+function toggleButtonState(inputList, buttonElement) {
+    if(hasInvalidInput(inputList)) {
+        buttonElement.disabled = true;
+        buttonElement.classList.add('popup__submit_inactive');
+    } else {
+        buttonElement.disabled = false;
+        buttonElement.classList.remove('popup__submit_inactive');
+    }
+}
+enableValidation();
